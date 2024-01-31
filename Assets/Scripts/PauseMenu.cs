e277a72c145a255cff2e48ade2caaddf39f960ac
Keegan Hauser <keeghauser08@gmail.com>
2024-01-30 20:40:37 -0800

    Added resume/quit buttons to pause menu

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,13 +2,24 @@ using MariaDBLib;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEditor.EditorTools;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
-    MariaDB db;
+    private MariaDB db;
+    private bool isPaused;
+    private Image image;
+
+    [SerializeField]
+    private TMP_Text pauseText;
+
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float pauseTextSpeed;
 
     [SerializeField]
     private Button saveButton;
@@ -19,16 +30,68 @@ public class PauseMenu : MonoBehaviour
     void Start()
     {
         db = FindObjectOfType<MariaDB>();
+        isPaused = false;
         saveButton.onClick.AddListener(SaveGame);
         loadButton.onClick.AddListener(LoadGame);
+        image = GetComponent<Image>();
+        image.enabled = isPaused;
+
+        // Activate/Deactivate all children
+        foreach (GameObject child in GetChildren())
+        {
+            child.SetActive(isPaused);
+        }
     }
 
     void Update()
     {
+        // Pause game when ESC is pressed
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            gameObject.SetActive(!gameObject.activeInHierarchy);
+            TogglePauseMenu();
         }
+
+        if (isPaused)
+        {
+            UpdatePauseText();
+        }
+    }
+
+    private IEnumerable<GameObject> GetChildren()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            yield return transform.GetChild(i).gameObject;
+        }
+    }
+
+    private void UpdatePauseText()
+    {
+        pauseText.color = Color.HSVToRGB(
+            Mathf.PingPong(Time.time * pauseTextSpeed, 1), 
+            1, 1);
+    }
+
+    private void TogglePauseMenu()
+    {
+        isPaused = !isPaused;
+
+        // Enable/disable panel's background image and all children
+        image.enabled = isPaused;
+        foreach (GameObject child in GetChildren())
+        {
+            child.SetActive(isPaused);
+        }
+    }
+
+    public void ResumeGame()
+    {
+        TogglePauseMenu();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     public void SaveGame()
