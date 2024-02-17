ae679758e580ccfaa3ed6ad5223c81ef06c9cb98
Keegan Hauser <keeghauser08@gmail.com>
2024-02-17 01:07:28 -0800

    Added test quest and one quest trigger mechanism
    Trigger mechanism uses OnTriggerEnter2D to trigger a specified quest as complete.

---
+++ Assets/Scripts/TriggerObjectiveOnTriggerEntry.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerObjectiveOnTriggerEntry : MonoBehaviour
+{
+    [SerializeField]
+    private ObjectiveTrigger objective = new ObjectiveTrigger();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            objective.Invoke();
+            this.enabled = false;
+        }
+    }
+}
